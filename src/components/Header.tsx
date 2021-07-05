e4d0dcbe8f203492b1bcc7cf152eda36207cc3c5
Iury Nogueira <iunog@hotmail.com>
2021-07-05 18:29:41 -0300

    :tada: Project with header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+
+  return(
+    <header style={{ display: 'flex', justifyContent: 'space-between', padding: '16px', background: '#f5eee3', color: '#2e343f' }}>
+      <div>Reef</div>
+      {/* <div>Sair</div> */}
+    </header>
+  )
+
+}
+
+export default Header;
