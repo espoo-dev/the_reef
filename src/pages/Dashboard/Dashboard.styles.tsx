3973a2facc90cac25e3d26b4a7587b864e28e49e
Iury Nogueira <iunog@hotmail.com>
2021-07-27 19:12:28 -0300

    :sparkles: Adding stylecomponents to improve styles and adding dashboard page with mocked cards #23

---
+++ src/pages/Dashboard/Dashboard.styles.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const HeaderSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0px 20px;
+  color: #464646;
+  background: #edfbfe;
+  align-items: center;
+`;
+
+export const ImgMonitor = styled.img`
+  height: 300px;
+`;
+
+export const CardsSection = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+  padding: 20px;
+`;
+
+// Card
+export const CardMonitor = styled.div`
+  padding: 10px;
+  color: #464646;
+  display: flex;
+  flex-direction: column;
+  border-radius: 6px;
+  box-shadow: -1px 12px 23px -10px rgb(0 0 0 / 15%);
+  border: 1px solid #eaeaea;
+`;
