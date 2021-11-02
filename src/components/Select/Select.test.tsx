d3b9df1ed81c4133d9d9a4026b441b8007acbf38
Iury Nogueira <iunog@hotmail.com>
2021-11-02 12:27:49 -0300

    :test_tube: Improve test select  #9

--- src/components/Select/Select.test.tsx
+++ src/components/Select/Select.test.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Select from './Select';
 
 const stateMock = jest.fn().mockReturnValue([{}, {}]);
+const options = [
+  { id: 1, name: 'Iury' },
+  { id: 2, name: 'Nogueira' },
+];
 
 describe('Select defaults tests', () => {
   it('should create and render select on screen', () => {
@@ -14,29 +18,21 @@ describe('Select defaults tests', () => {
 
   it('should render select with options', () => {
     render(
-      <Select
-        options={[{ id: '1', name: 'iury' }]}
-        option={{}}
-        setOptionSelected={stateMock}
-      />
+      <Select options={options} option={{}} setOptionSelected={stateMock} />
     );
     const selectMain = screen.getByTestId('select-main');
     fireEvent.click(selectMain);
-    expect(screen.getByText('iury')).toBeTruthy();
+    expect(screen.getByText(options[0].name)).toBeTruthy();
   });
 
   it('should select a option', () => {
     render(
-      <Select
-        options={[{ id: '1', name: 'iury' }]}
-        option={{}}
-        setOptionSelected={stateMock}
-      />
+      <Select options={options} option={{}} setOptionSelected={stateMock} />
     );
     const selectMain = screen.getByTestId('select-main');
     fireEvent.click(selectMain);
-    const option = screen.getByText('iury');
+    const option = screen.getByText(options[0].name);
     fireEvent.click(option);
-    expect(stateMock).toHaveBeenCalledWith({ id: '1', name: 'iury' });
+    expect(stateMock).toHaveBeenCalledWith(options[0]);
   });
 });
