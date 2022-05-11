c97f5d2f0663869c1434eb2df0f4830d09646fa8
Iury Nogueira <iunog@hotmail.com>
2022-05-11 20:01:35 -0300

    :test_tube: add test to aquarium entity to create, get liters and add finish #26

--- back/test/unit/Aqua.test.ts
+++ back/test/unit/Aqua.test.ts
@@ -1,6 +1,20 @@
 describe('Aquarium', () => {
   it('should create an aquarium', () => {
-    const aquarium = new Aquarium('Aquarium 1');
-    expect(aquarium.name).toBe('Aquarium 1');
+    const aquarium = new Aquarium('My Reef');
+    expect(aquarium.name).toBe('My Reef');
+  });
+
+  it('should return total liters the aquarium', () => {
+    const dimensions = new Dimensions(10, 10, 10);
+    const aquarium = new Aquarium('My Reef', dimensions);
+    expect(aquarium.getLiters()).toBe(125);
+  });
+
+  it('should add a fish', () => {
+    const dimensions = new Dimensions(10, 10, 10);
+    const aquarium = new Aquarium('My Reef', dimensions);
+    const fish = new Fish('Nemo', 'clownfish', 1);
+    aquarium.addFish(fish);
+    expect(aquarium.getFishs()).toBe([{ name: 'Nemo', type: 'clownfish' }]);
   });
 });
