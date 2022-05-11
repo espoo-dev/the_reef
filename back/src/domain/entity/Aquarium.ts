6a8678e763f10ba90cfb9503954784f71efa3ed6
Iury Nogueira <iunog@hotmail.com>
2022-05-11 20:34:20 -0300

    :sparkles: add entity fish, add, list and remove fish to aquarium #26

--- back/src/domain/entity/Aquarium.ts
+++ back/src/domain/entity/Aquarium.ts
@@ -1,14 +1,37 @@
 import Dimensions from "./Dimensions";
+import Fish from "./Fish";
 
 export default class Aquarium {
   name: string;
   dimensions?: Dimensions;
+  fishs: Fish[] = [];
 
   constructor(name: string, dimensions?: Dimensions) {
     this.name = name;
+    this.dimensions = dimensions;
   }
 
   getLiters(): number {
+    if (!this.dimensions) {
+      throw new Error("Aquarium dont have dimensions");
+    }
     return this.dimensions.getLiters();
   }
+
+  addFish(fish: Fish): void {
+    this.fishs.push(fish);
+  }
+
+  getFishs(): Fish[] {
+    return this.fishs;
+  }
+
+  removeFish(name: string): void {
+    this.fishs.forEach((fish, index) => {
+      if (fish.name === name) {
+        this.fishs.splice(index, 1);
+      }
+    })
+  }
+
 }
--- back/test/unit/Aqua.test.ts
+++ back/test/unit/Aqua.test.ts
@@ -1,5 +1,6 @@
 import Aquarium from "../../src/domain/entity/Aquarium";
 import Dimensions from "../../src/domain/entity/Dimensions";
+import Fish from "../../src/domain/entity/Fish";
 
 describe('Aquarium', () => {
   it('should create an aquarium', () => {
@@ -8,16 +9,31 @@ describe('Aquarium', () => {
   });
 
   it('should return total liters the aquarium', () => {
-    const dimensions = new Dimensions(10, 10, 10);
+    const dimensions = new Dimensions(50, 50, 50);
     const aquarium = new Aquarium('My Reef', dimensions);
     expect(aquarium.getLiters()).toBe(125);
   });
 
-  it.skip('should add a fish', () => {
+  it('should return error when dont have dimensions', () => {
+    const aquarium = new Aquarium('My Reef');
+    expect(() => aquarium.getLiters()).toThrowError('Aquarium dont have dimensions');
+  })
+
+  it('should add a new fish', () => {
     const dimensions = new Dimensions(10, 10, 10);
     const aquarium = new Aquarium('My Reef', dimensions);
-    const fish = new Fish('Nemo', 'clownfish', 1);
+    const fish = new Fish('Nemo', 'clownfish', 10);
     aquarium.addFish(fish);
-    expect(aquarium.getFishs()).toBe([{ name: 'Nemo', type: 'clownfish' }]);
+    expect(aquarium.getFishs()).toContain(fish);
   });
+
+  it('should remove a fish', () => {
+    const aquarium = new Aquarium('My Reef');
+    const fish = new Fish('Nemo', 'clownfish', 10);
+    aquarium.addFish(fish);
+    aquarium.removeFish('Nemo');
+    expect(aquarium.getFishs()).not.toContain(fish);
+  })
+
+  it.skip('should add a new fish only when have space', () => {})
 });
