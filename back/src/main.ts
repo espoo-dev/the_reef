28448633ace393e5ab2df8192116a9a03ef4545e
Iury Nogueira <iunog@hotmail.com>
2022-06-01 18:43:47 -0300

    :sparkles: feat: add controller to aquarium routes #88

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,3 +1,4 @@
+import AquariumController from './infra/controller/AquariumController'
 import { PgPromiseConnectionAdapter } from './infra/database'
 import { ExpressAdapter } from './infra/http/ExpressAdapter'
 import { AquariumRepositoryDatabase } from './infra/repository/database'
@@ -5,11 +6,8 @@ import { AquariumRepositoryDatabase } from './infra/repository/database'
 const http = new ExpressAdapter()
 const connection = new PgPromiseConnectionAdapter()
 
-http.on('get', '/aquariums', async () => {
-  const aquariumRepository = new AquariumRepositoryDatabase(connection)
-  const output = await aquariumRepository.list()
-  return output
-})
+const aquariumRepository = new AquariumRepositoryDatabase(connection)
+new AquariumController(http, aquariumRepository)
 
 http.on('get', '/', () => {
   return {
