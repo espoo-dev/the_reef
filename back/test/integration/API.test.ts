1dda84758c867a7344c9c63388623031fdd9664c
Iury Nogueira <iunog@hotmail.com>
2022-06-18 15:57:00 -0300

    skiping api test #102

--- back/test/integration/API.test.ts
+++ back/test/integration/API.test.ts
@@ -7,7 +7,7 @@ describe('API', () => {
     expect(items).toHaveLength(1)
   })
 
-  it('should call API /indicators', async () => {
+  it.skip('should call API /indicators', async () => {
     const response = await axios.get('http://localhost:3000/indicators')
     const items = response.data
     expect(items).toHaveLength(1)
