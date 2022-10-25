07ead5d98a36b5948f020bfa8d7dc6f722de5df6
Chrislayne <84159430+chrislaynexavier@users.noreply.github.com>
2022-10-25 20:31:54 -0300

    Create fish tests cases (#117)
    * add test to fish
    
    * add fish test

---
+++ back/test/unit/Fish.test.ts
@@ -0,0 +1,36 @@
+import { Fish } from "../../src/domain/entity"
+
+describe('Fish',() => {
+  let fish!:Fish
+  const mockFish = {
+    name: 'Sargentinho',
+    type: 'sea',
+    litersRequired: 40
+  }
+  
+  const sut = (mock: Fish = mockFish): Fish => {
+    return new Fish(mock.name, mock.type, mock.litersRequired)
+  }
+
+  beforeEach(() => {
+    fish = sut()
+  })
+
+  it('should create a new fish with correct name', () => {
+    expect(fish.name).toBe(mockFish.name) 
+  });
+
+  it('should create a new fish with other name', () => {
+    let fishName = 'ocellaris'
+    let newFish = sut({name: fishName, litersRequired: 30, type: 'fresh'})
+    expect(newFish.name).toBe(fishName) 
+  });
+
+  it('should has the correct type', () => {
+    expect(fish.type).toBe(mockFish.type)
+  });
+
+  it('should has the correct liters required', () => {
+    expect(fish.litersRequired).toBe(mockFish.litersRequired)
+  });
+})
