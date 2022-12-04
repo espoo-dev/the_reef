72fb8d9c4efcf65bc335d4d295659e5bb8b9fabd
Iury Nogueira <iunog@hotmail.com>
2022-12-04 16:26:22 -0300

    :sparkles: feat: add indicator endpoint (#122)
    * :sparkles: feat: add indicator endpoint
    
    * fix: lint

--- back/src/infra/controller/IndicatorController.ts
+++ back/src/infra/controller/IndicatorController.ts
@@ -1,15 +1,33 @@
 import { UpdateIndicatorValue } from '../../application/UpdateIndicatorValue'
 import RepositoryFactory from 'domain/factory/RepositoryFactory'
 import { Http } from 'infra/http'
+import { Indicator } from '../../domain/entity'
 
 export class IndicatorController {
   constructor (
     readonly http: Http,
     readonly repositoryFactory: RepositoryFactory
   ) {
+    const indicatorRepository = repositoryFactory.createIndicatorRepository()
+
     http.on('get', '/indicators', async () => {
-      const indicatorRepository = repositoryFactory.createIndicatorRepository()
       const output = await indicatorRepository.list()
+      return output
+    })
+
+    http.on('post', '/indicators', async (params: any, body: AddIndicatorInput) => {
+      const newIndicator = new Indicator(
+        body.id,
+        body.aquariumId,
+        body.name,
+        body.unit,
+        body.description,
+        body.currentValue,
+        body.acceptedValue,
+        body.minValue,
+        body.maxValue
+      )
+      const output = await indicatorRepository.save(newIndicator)
       return output
     })
 
@@ -19,4 +37,15 @@ export class IndicatorController {
       return output
     })
   }
+}
+
+interface AddIndicatorInput {
+  aquariumId: number
+  name: string
+  unit: string
+  description: string
+  currentValue: number
+  acceptedValue: number
+  minValue: number
+  maxValue: number
 }
--- back/test/integration/API.test.ts
+++ back/test/integration/API.test.ts
@@ -3,19 +3,36 @@ import axios from 'axios'
 const serverUrl = `${process.env.SERVER_URL}:${process.env.PORT}`
 
 describe('API', () => {
-  it.skip('should call API /indicators', async () => {
-    const response = await axios.get(`${serverUrl}/indicators`)
-    const items = response.data
-    expect(items).toHaveLength(1)
-  })
+  describe('Indicator', () => {
+    it.skip('should call API /indicators', async () => {
+      const response = await axios.get(`${serverUrl}/indicators`)
+      const items = response.data
+      expect(items).toHaveLength(1)
+    })
 
-  it.skip('should call API /indicator/id/update to update current value of indicator', async () => {
-    const indicators = await axios.get(`${serverUrl}/indicators`)
-    const requestBody = { newValue: 20, indicatorId: indicators.data[0].id }
-    const response = await axios.put(`${serverUrl}/indicators/update`, requestBody)
-    const indicator = response.data
-    expect(indicator.currentValue).toBe(requestBody.newValue)
-  })
+    it.skip('should call API /indicator/id/update to update current value of indicator', async () => {
+      const indicators = await axios.get(`${serverUrl}/indicators`)
+      const requestBody = { newValue: 20, indicatorId: indicators.data[0].id }
+      const response = await axios.put(`${serverUrl}/indicators/update`, requestBody)
+      const indicator = response.data
+      expect(indicator.currentValue).toBe(requestBody.newValue)
+    })
+
+    it.skip('should call API POST /indicators to add a new Indicator', async () => {
+      const newIndicator = {
+        aquariumId: 1,
+        name: 'Temperature',
+        unit: 'celsius',
+        description: 'Temperature of the reef',
+        currentValue: 27,
+        acceptedValue: 26,
+        minValue: 26,
+        maxValue: 29
+      }
+      const response = await axios.post(`${serverUrl}/indicators`, newIndicator)
+      expect(response.data.name).toBe(newIndicator.name)
+    })
+  });
 
   describe('Aquarium', () => {
     it.skip('should call API /aquariums', async () => {
