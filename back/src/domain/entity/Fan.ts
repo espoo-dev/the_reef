6c7412d50aa35343d95d19de09790f8725494eb5
Iury Nogueira <iunog@hotmail.com>
2022-12-22 18:23:45 -0300

    feat: create fan entity :sparkles:

---
+++ back/src/domain/entity/Fan.ts
@@ -0,0 +1,16 @@
+export class Fan {
+  name: string;
+  on: boolean = false;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  turnOn(): void {
+    this.on = true;
+  }
+
+  turnOff(): void {
+    this.on = false;
+  }
+}
