8fd7f200978618bf9489f73315fc483b88875901
Iury Nogueira <iunog@hotmail.com>
2022-12-22 18:31:20 -0300

    lint: fix :hammer:

--- back/src/domain/entity/Fan.ts
+++ back/src/domain/entity/Fan.ts
@@ -2,15 +2,15 @@ export class Fan {
   name: string;
   on: boolean = false;
 
-  constructor(name: string) {
-    this.name = name;
+  constructor (name: string) {
+    this.name = name
   }
 
-  turnOn(): void {
-    this.on = true;
+  turnOn (): void {
+    this.on = true
   }
 
-  turnOff(): void {
-    this.on = false;
+  turnOff (): void {
+    this.on = false
   }
 }
