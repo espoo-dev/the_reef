48a9f41a7c4fab46891e472dde3cecd9ee5b823e
Iury Nogueira <iunog@hotmail.com>
2024-06-10 11:03:00 -0300

    feat: add sensor component and refactor home page to use the new component in grid layout :sparkles:

--- thereef/src/app/pages/home/home.component.ts
+++ thereef/src/app/pages/home/home.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
+import { SensorComponent, SensorStatusType, SensorType } from '../../components/sensor/sensor.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, SensorComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  public sensors: SensorType[] = [
+    {
+      value: '27.88 °C',
+      name: 'Temperatura',
+      lastUpdate: '09/06/24 19:28',
+      status: 'success'
+    },
+    {
+      value: '30.88 °C',
+      name: 'Temperatura',
+      lastUpdate: '10/06/24 19:33',
+      status: 'danger'
+    },
+    {
+      value: '30.88 °C',
+      name: 'Temperatura',
+      lastUpdate: '10/06/24 19:33',
+      status: 'danger'
+    },
+    {
+      value: '27.88 °C',
+      name: 'Temperatura',
+      lastUpdate: '09/06/24 19:28',
+      status: 'success'
+    },
+    {
+      value: '30.88 °C',
+      name: 'Temperatura',
+      lastUpdate: '10/06/24 19:33',
+      status: 'danger'
+    },
+    {
+      value: '27.88 °C',
+      name: 'Temperatura',
+      lastUpdate: '09/06/24 19:28',
+      status: 'success'
+    },
+  ];
 }
