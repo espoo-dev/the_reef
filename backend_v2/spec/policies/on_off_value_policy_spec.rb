7ba67ef1cd46f411ae9f2be23ac40450b9a6836c
Edimo Silva <edimossilva@gmail.com>
2024-08-26 14:34:16 -0300

    Fix cops

--- backend_v2/spec/policies/on_off_value_policy_spec.rb
+++ backend_v2/spec/policies/on_off_value_policy_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe OnOffValuePolicy do
   subject { described_class.new(user, on_off_value) }
@@ -6,13 +8,13 @@ RSpec.describe OnOffValuePolicy do
   let(:on_off_value) { build(:on_off_value) }
   let(:owner_user) { on_off_value.on_off_actuator.aquarium.user }
 
-  context 'when user is not owner' do
+  context "when user is not owner" do
     let(:user) { build(:user) }
 
     it { is_expected.to forbid_all_actions }
   end
 
-  context 'when user is owner' do
+  context "when user is owner" do
     let(:user) { owner_user }
 
     it { is_expected.to permit_all_actions }
